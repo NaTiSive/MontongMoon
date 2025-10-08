57b0d260a10409abf8962ecbeb1bf62ac2af3485
NaTiSive <Nattakorn4082@gmail.com>
2025-10-08 17:11:34 +0700

    add pages folder and reset index and App .css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,11 @@
 import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
 
 function App() {
-  const [data, setData] = useState({})
-
-  useEffect(() => {
-
-    axios.get('http://localhost:3000/api/data').then(res => {
-      setData(res.data);
-    }).catch(err => {
-      console.error("Error fetching data: ", err)
-    })
-
-  }, [])
-
   return (
-    <>
-      <div className='text-xl font-semibold text-blue-600 bg-gray-100 p-4 rounded-lg shadow'>
-        {data.message}
-      </div>
-    </>
+    <div></div>
   )
 }
 
