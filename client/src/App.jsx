1916e9ac179b3c83be3921ae6b1d0934b23bca28
NaTiSive <Nattakorn4082@gmail.com>
2025-10-08 17:04:44 +0700

    Create pageheader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,27 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
+import PageHeader from './components/pageHeader'
 
 function App() {
-  const [data, setData] = useState({})
-
-  useEffect(() => {
-
-    axios.get('http://localhost:3000/api/data').then(res => {
-      setData(res.data);
-    }).catch(err => {
-      console.error("Error fetching data: ", err)
-    })
-
-  }, [])
-
   return (
-    <>
-      <div className='text-xl font-semibold text-blue-600 bg-gray-100 p-4 rounded-lg shadow'>
-        {data.message}
-      </div>
-    </>
-  )
+    <div>
+      <PageHeader />
+    </div>
+  );
 }
+
 
 export default App
---
+++ client/src/components/PageHeader.jsx
@@ -0,0 +1,10 @@
+export default function PageHeader(){
+    return(
+        <div className="p-7">
+            <div className="mb-6">
+                <h1 className="text-3xl font-semibold text-gray-800">หัวข้อหน้า</h1>
+                <p className="text-sm mt-1 text-gray-600">รายละเอียดหน้า</p>
+            </div>
+        </div>
+    );
+}
