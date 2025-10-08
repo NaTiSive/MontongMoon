61632dbb976faee3120f2518f808a7cbcd7d070f
NaTiSive <Nattakorn4082@gmail.com>
2025-10-08 20:50:14 +0700

    create InputField components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,13 @@ import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import axios from 'axios'
+import InputField from './components/InputField'
 
 function App() {
   return (
-    <div></div>
+    <div>
+       <InputField label="ชื่อ" placeholder="กรอกชื่อของคุณ" />
+    </div>
   )
 }
 
