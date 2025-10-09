12be72c90c36118466640539798cdaceb4aa87a8
NaTiSive <Nattakorn4082@gmail.com>
2025-10-09 20:43:41 +0700

    create serchbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,18 @@ import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import axios from 'axios'
+import SearchBar from './components/serchbar'
 
 function App() {
+  const handleSearch = (keyword) => {
+    console.log("คำที่ค้นหา:", keyword);
+  };
+
   return (
-    <div></div>
-  )
+    <div className="p-6">
+      <SearchBar placeholder="ค้นหาชื่อผู้รับเหมา..." onSearch={handleSearch} />
+    </div>
+  );
 }
 
 export default App
