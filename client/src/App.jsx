3050b807bd7fa41d57c1f9b6b2c228722ba66141
NaTiSive <Nattakorn4082@gmail.com>
2025-10-09 15:18:18 +0700

    create textarea component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-import { useState, useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import axios from "axios";
+import TextArea from "./components/TextArea";
 
 function App() {
+  const [problem, setProblem] = useState("");
+
   return (
-    <div></div>
-  )
+    <div>
+      <TextArea
+        label="รายละเอียดปัญหา"
+        placeholder="ตัวอย่าง: ใบเหลือง"
+        value={problem}
+        onChange={(e) => setProblem(e.target.value)}
+        rows={4}
+      />
+
+      <p className="mt-3 text-gray-700">ค่าที่ป้อนคือ: {problem}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
